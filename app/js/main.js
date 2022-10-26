a0eae830bb27fb0bcb5469836757a4b41c2cfd3a
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2022-10-26 14:15:11 +0300

    slider info

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,12 @@
-$(function(){
+$(function () {
 
-$('.header__slider').slick({
-    infinite: true,
-    fade: true
-})
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""><img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""><img>'
 
-  
+    })
+
+
 });
